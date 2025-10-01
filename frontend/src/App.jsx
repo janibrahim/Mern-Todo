c010cddc6b67bdb0ee7e19cc1afb954b132166a5
Jan Ibrahim <janeditz8@gmail.com>
2025-10-01 14:20:16 +0500

    this is simple commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,12 @@ function App() {
         <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
           Click Me
         </button>
+        <button>
+          <a href="https://example.com" target="_blank" rel="noopener noreferrer">Go to Example.com</a> 
+        </button>
+        <button>
+          <a href="https://example.com" target="_blank" rel="noopener noreferrer">Go to Example.</a> 
+        </button>
       </div>
     </>
   );
